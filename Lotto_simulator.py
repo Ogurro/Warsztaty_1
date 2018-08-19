49528cc71aa3410bdcebbb2af31fcae754528a92
Ogurro <ogur.pawel@gmail.com>
2018-08-19 20:52:26 +0200

    update lotto simulator

--- Lotto_simulator.py
+++ Lotto_simulator.py
@@ -5,7 +5,7 @@ def generate_winning_numbers():
     return random.sample(range(1, 50), 6)
 
 
-def check_input(check_numb):
+def check_input(check_numb, numb_list):
     try:
         check_numb = int(check_numb)
     except ValueError:
@@ -14,31 +14,35 @@ def check_input(check_numb):
         return check_numb not in numb_list and 0 < check_numb < 50
 
 
-def check_win(check_numbs, win_numbs):
+def check_win(check_numbs, numbs):
     rv = 0
     for i in range(6):
-        if check_numbs[i] in win_numb:
+        if check_numbs[i] in numbs:
             rv += 1
-    return rv
-
-
-numb_list = []
-i = 1
-while i < 7:
-    user_numb = input(f'Insert {i} number: ')
-    if check_input(user_numb):
-        numb_list.append(int(user_numb))
-        i += 1
+    if rv > 2:
+        print(f'Winner, You got {rv} numbers!')
     else:
-        print('Wrong input, try again')
-numb_list = sorted(numb_list)
-print("Your numbers: {}".format(' '.join(map(str, numb_list))))
+        print(f'sorry, You got {rv} numbers')
 
-win_numb = sorted(generate_winning_numbers())
-print("Wining numbers {}".format(' '.join(map(str, win_numb))))
 
-hits = check_win(numb_list, win_numb)
-if hits > 2:
-    print(f'Winner, You got {hits} numbers!')
-else:
-    print(f'sorry, You missed {6-hits} numbers')
+def get_user_numbers():
+    numb_list = []
+    i = 1
+    while i < 7:
+        user_numb = input(f'Insert {i} number: ')
+        if check_input(user_numb, numb_list):
+            numb_list.append(int(user_numb))
+            i += 1
+        else:
+            print('Wrong input, try again')
+    return numb_list
+
+
+if __name__ == '__main__':
+    user_numbers = sorted(get_user_numbers())
+    win_numb = sorted(generate_winning_numbers())
+
+    print("Your numbers: {}".format(' '.join(map(str, user_numbers))))
+    print("Wining numbers {}".format(' '.join(map(str, win_numb))))
+
+    check_win(user_numbers, win_numb)
